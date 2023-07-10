a3ac86b13d4030d5c2020996811125d231c0fd2a
Carlos Marques <kadupmarques@gmail.com>
2023-07-10 20:23:22 -0300

    feat: Implement Footer Style and Content

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import styles from '@/app/styles/footer.module.css'
+import React from "react";
+import styles from "@/app/styles/footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>Footer</div>
+      <p>Versão para Teste - Carlos Marques 2023 &copy; </p>
+      <Link href="https://github.com/CarlosEduardoPereiraMarques/sou-odonto-teste/">
+        <Image
+          src="github-mark-white.svg"
+          alt="github-mark"
+          width={40}
+          height={40}
+          title="Clique para acessar o meu github"
+        />
+      </Link>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
