62fd8b665fd0b87420ffb418c7e317961e1e3c70
Carlos Marques <kadupmarques@gmail.com>
2023-07-10 21:26:06 -0300

    fix: Fixing Footer over vertical scroll

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,17 @@
-import { Inter } from 'next/font/google'
 import './styles/globals.css'
 import style from '@/app/styles/container.module.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/footer'
 
 
-const inter = Inter({ subsets: ['latin'] })
-
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>
+      <body className={style.body}>
         <Navbar/>
-        <div className={style.container}>
+        <main className={style.container}>
             {children}
-        </div>
+        </main>
         <Footer/>
       </body>
     </html>
