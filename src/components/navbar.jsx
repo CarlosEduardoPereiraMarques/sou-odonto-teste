58a9409ced3d069f4806fe713455b77ef5ef9959
Carlos Eduardo Pereira Marques <784292@pucminas.br>
2023-07-11 08:41:34 -0300

    Revert "issue: Try to make navebar responsive works and dynamic logo images"
    This reverts commit 4aaeb60ae535001dd9ed56ec13676418cddf1c9d.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,102 +1,48 @@
-"use client";
+import React from 'react'
+import Link from 'next/link'
+import style from '@/app/styles/navbar.module.css'
+import Image from 'next/image';
 
-import React, { useState, useEffect } from "react";
-import Link from "next/link";
-import style from "@/app/styles/navbar.module.css";
-import Image from "next/image";
-
-const links = [
-  {
+const links = [{
     id: 1,
     title: "Acadêmicos",
     url: "/categories/academicos",
-  },
-  {
+  }, {
     id: 2,
     title: "Dentistica",
     url: "/categories/dentistica",
-  },
-  {
+  }, {
     id: 3,
     title: "Descartáveis",
     url: "/categories/descartaveis",
-  },
-  {
+  }, {
     id: 4,
     title: "Endodontia",
     url: "/categories/endodontia",
-  },
-  {
+  },{
     id: 5,
     title: "Listas de Compras",
     url: "/account/listas-de-compras",
-  },
+  }
 ];
 
 const userData = {
-  id: 1,
-  title: "Meus Dados",
-  url: "/account/meus-dados",
-};
+    id: 1,
+    title: "Meus Dados",
+    url: "/account/meus-dados",
+}
 
 const Navbar = () => {
-  const [windowWidth, setWindowWidth] = useState(0);
-  const [logoImage, setLogoImage] = useState("/imgs/SouOdontoSymbol.png");
-  const [logoSize, setLogoSize] = useState({ width: 250, height: 70 });
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  useEffect(() => {
-    if (windowWidth >= 1050) {
-      setLogoImage("/imgs/SouOdontoMarketplace.png");
-      setLogoSize({ width: 300, height: 85 });
-    } else {
-      setLogoImage("/imgs/SouOdontoSymbol.png");
-      setLogoSize({ width: 250, height: 70 });
-    }
-  }, [windowWidth]);
-
   return (
     <header className={style.navbar}>
       <div className={`${style.column} ${style.logoColumn}`}>
         <Link href="/">
-          <div className={style.logoImg}>
-            {windowWidth >= 1050 ? (
-              <Image
-                src={logoImage}
-                alt="Sou Odonto Logo"
-                width={300}
-                height={85}
-              />
-            ) : (
-              <Image
-                src={logoImage}
-                alt="Sou Odonto Logo"
-                width={250}
-                height={70}
-              />
-            )}
-          </div>
+          <Image src="/imgs/SouOdontoMarketPlace.png" width={250} height={70} alt="Sou Odonto Logo" />
         </Link>
       </div>
-
       <div className={`${style.column} ${style.searchColumn}`}>
         <div>
-          <input
-            type="text"
-            name="SearchField"
-            id="searchField"
-            className={style.search}
-            placeholder="Pesquisar um Produto"
-          />
+          <input type="text" name="SearchField" id="searchField" className={style.search} placeholder='Pesquisar um Produto'/>
         </div>
         <div className={style.categories}>
           <ul>
@@ -111,11 +57,11 @@ const Navbar = () => {
       <div className={`${style.column} ${style.userColumn}`}>
         <div className={style.userInfo}>
           <div className={style.userInfoText}>
-            <div className={style.userInfoLogin}>
+            <div>
               <Link href="/login">Faça o login</Link>
             </div>
             <hr className={style.divider} />
-            <div className={style.userInfoRegister}>
+            <div>
               <Link href="/register">Crie sua conta</Link>
             </div>
           </div>
